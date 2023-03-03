2f440624892e0d15d45653c6cbe7f6f6eac62467
paul <paulkelechi11@gmail.com>
2023-03-02 20:08:57 -0800

    Created some methods for modifying objects in console

--- console.py
+++ console.py
@@ -2,12 +2,97 @@
 """A console to handle creation, deletion, updating of objects on website"""
 import cmd
 
+"""import classes used"""
+from models.base_model import BaseModel
+import models
+
+"""Instantiators - a dictionary containing all classes"""
+instantiators = {"BaseModel": BaseModel}
+
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for the hbnb website"""
 
     prompt = '(hbnb) '
 
+    def do_create(self, classname):
+        """creates an instance of the classname"""
+        if  not classname:
+            return print("** class name missing **")
+        if not instantiators[classname]:
+            return print("** class doesn't exist **")
+        else:
+            new_base = instantiators[classname]()
+            print(new_base.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an instance based
+        on the class name and id"""
+        if not line:
+            return print("** class name missing **")
+        if len(line.split()) < 2:
+            return print("** instance id missing **")
+
+        classname, id = line.split()
+        if not classname in instantiators:
+            return print("** class doesn't exist **")
+        str = models.storage.view(classname, id)
+        if not str:
+            return print("** no instance found **")
+        print(instantiators[classname](**str))
+
+    def do_destroy(self, line):
+        """ Deletes an instance based on the class name and id"""
+        if not line:
+            return print("** class name missing **")
+        if len(line.split()) < 2:
+            return print("** instance id missing **")
+
+        classname, id = line.split()
+        if not classname in instantiators:
+            return print("** class doesn't exist **")
+        done = models.storage.delete(classname, id)
+        if not done:
+            return print("** no instance found **")
+
+    def do_all(self, classname):
+        """ Prints all string representation of all instances
+        based or not on the class name"""
+        all_list = []
+        if classname != "":
+            if not classname in instantiators:
+                return print("** class doesn't exist **")
+            all_dict = models.storage.all()
+            for key, value in all_dict.items():
+                if value["__class__"] == classname:
+                    all_list.append(f'{str(instantiators[classname](**value))}')
+        else:
+            all_dict = models.storage.all()
+            #print(all_dict)
+            for key, value in all_dict.items():
+                if value["__class__"] in instantiators:
+                    all_list.append(f'{str(instantiators[value["__class__"]](**value))}')
+        print(all_list)
+
+    def do_update(self, line):
+        """Updates an instance based on the class name and id using attributes"""
+        if not line:
+            return print("** class name missing **")
+        args = line.split()
+        if args[0] not in instantiators:
+                return print("** class doesn't exist **")
+        if len(args) == 1:
+            return print("** instance id missing **")
+        if not models.storage.exists(args[0], args[1]):
+            return print("** no instance found **")
+        if len(args) == 2:
+            return print("** attribute name missing **")
+        if len(args) == 3:
+            return print("** value missing **")
+        classname, id, attr_name, attr_value, *_ = args
+        models.storage.update(classname, id, attr_name, attr_value)
+
     def emptyline(self):
+        """handle when an empty line is sent to interpreter"""
         print
 
     def do_quit(self, line):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,14 +20,36 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
+        #print("**[Inside file_storage.py] [Inside new method]**")
         obj_name = f'{obj["__class__"]}.{obj["id"]}'
         self.__objects[obj_name] = obj
+
+    def update(self, classname, id, attr_name, attr_value):
+        if attr_name in self.__objects[f'{classname}.{id}']:
+            attr_type = type(self.__objects[f'{classname}.{id}'][attr_name])
+            self.__objects[f'{classname}.{id}'][attr_name] = attr_type(attr_value)
+        self.__objects[f'{classname}.{id}'][attr_name] = attr_value
+
+    def view(self, classname, id):
+        #print(f'**__objects = {self.__objects} classname = {classname}, id = {id}')
+        if f'{classname}.{id}' in self.__objects:
+            return self.__objects[f'{classname}.{id}']
+
+    def delete(self, classname, id):
+        """Deletes an entry from __objects based on classname.id as key"""
+        if f'{classname}.{id}' in self.__objects:
+            del self.__objects[f'{classname}.{id}']
+            self.save()
+            return True
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_file = open(self.__file_path, "w")
         json.dump(self.__objects, json_file)
         json_file.close()
+
+    def exists(self, classname, id):
+        return f'{classname}.{id}' in self.__objects
 
     def reload(self):
         """deserializes the JSON file to __objects\
